4f596dedf65d61ed34ab972d3e324d276f548c7e
Alina Leonova <alinaa.leonova@gmail.com>
2020-11-03 00:54:28 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,60 +1,111 @@
 import './styles.css';
+import debounce from "../node_modules/lodash.debounce/index.js";
+import { error, defaultModules } from '../node_modules/@pnotify/core';
+import * as PNotifyDesktop from '../node_modules/@pnotify/desktop';
+import '../node_modules/@pnotify/core/dist/BrightTheme.css';
+import "../node_modules/@pnotify/core/dist/PNotify.css";
 
 
-const body = document.querySelector('body')
 const inputContainer = document.querySelector('.input-container');
 const countryList = document.querySelector('.country-list');
 const clearBtn = document.querySelector('.clear-btn');
+const searchResult = document.querySelector('.result-search');
 
-inputContainer.addEventListener('change', () => {
+defaultModules.set(PNotifyDesktop, {});
+
+searchResult.innerHTML = localStorage.getItem('country') || '';
+
+inputContainer.value = "";
+
+const inputFn = function () {
+    if (inputContainer.value == '') {
+        countryList.innerHTML = "";
+    }
+
     fetch(`https://restcountries.eu/rest/v2/name/${inputContainer.value}`)
         .then(data => data.json())
         .then(data => {
-
+            countryList.innerHTML = "";
             data.forEach(element => {
                 if (data.length === 1) {
-                    body.insertAdjacentHTML('beforeend', `
-      <div>
-      <h2 class="strana">${element.name}</h2>
-      <img class="flag" width="200" src="${element.flag}">
-      <ul class="proStarnu">
+                    searchResult.innerHTML = "";
+                    searchResult.insertAdjacentHTML('beforeend', `
+      
+      <h2 class="">${element.name}</h2>
+      <img width="200" src="${element.flag}">
+      <ul>
       <li>Capital: ${element.capital}</li>
       <li>Population: ${element.population}</li>
       <li>Languages:
+      <ul class="lang-list"></ul>
       </li>
       </ul>
-      </div>`)
+      `);
+                    localStorage.setItem('country', searchResult.innerHTML);
+                    element.languages.forEach(({ name }) => {
+                        document.querySelector('.lang-list').insertAdjacentHTML('beforeend', `<li>${name}</li>`)
+                    })
                 }
 
-                if (data.length > 1) {
+                if (data.length > 1 && data.length < 10) {
 
                     countryList.insertAdjacentHTML('afterbegin', `<li class="countryListItem">${element.name}</li>`)
                     document.querySelector('.countryListItem').addEventListener('click', () => {
-                        body.insertAdjacentHTML('beforeend', `
-      <div>
-      <h2 class="strana">${element.name}</h2>
+                        searchResult.innerHTML = '';
+                        searchResult.insertAdjacentHTML('beforeend', `
+      
+      <h2 class="">${element.name}</h2>
       <img class="flag" width="200" src="${element.flag}">
       <ul class="proStarnu">
       <li>Capital: ${element.capital}</li>
       <li>Population: ${element.population}</li>
       <li>Languages:
+      <ul class="lang-list"></ul>
       </li>
       </ul>
-      </div>`)
+      `);
+                        localStorage.setItem('country', searchResult.innerHTML);
+                        element.languages.forEach(({ name }) => {
+                            document.querySelector('.lang-list').insertAdjacentHTML('beforeend', `<li>${name}</Li>`)
+                        })
 
                     })
                 };
+
+                if (data.length > 10) {
+                    const errNotice2 = error({
+                        title: 'Too much result',
+                        text: "Please, try again",
+                        delay: 1700,
+                        modules: new Map([
+                            ...defaultModules,
+                            [PNotifyDesktop, {}
+                            ]])
+                    })
+                    return
+                }
             })
         })
-})
+        .catch(err => {
+            const errNotice1 = error({
+                title: "Enter correct country",
+                text: "Please, try again",
+                delay: 1700,
+                modules: new Map([
+                    ...defaultModules,
+                    [PNotifyDesktop, {}]
+                ])
 
+            })
+        })
+}
+
+inputContainer.addEventListener('input', debounce(inputFn, 500));
 
 const cleaner = function () {
     inputContainer.value = '';
 }
 
-// const cleanerList = function () {
-//     document.querySelector('.countryList').removeChild();
-// }
-
 clearBtn.addEventListener('click', cleaner);
+
+
